26d34397ec4966f254ea6bb4d477c8bb1ff6cdf4
Maven <66513443+Hejian-feng@users.noreply.github.com>
2020-06-12 18:22:35 +0800

    practice of java programming language
    初级语法练习及eclipse使用

---
+++ project/test01.java
@@ -0,0 +1,54 @@
+package com.test.test01;
+
+import java.util.Scanner;
+
+public class test01 {
+	
+//输出乘法口诀表
+//	public static void main(String[] args) {
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print(i + "*" + j + "=" + (i * j) + "\t");
+//			}
+//			System.out.println();
+//		}
+//
+//	}
+	
+	
+//输出菱形
+	
+//		public static void main(String[] args) {
+//		Scanner s = new Scanner(System.in);
+//		int num = s.nextInt();
+//		for(int i=1;i<=num;i++) {
+//			for(int j=0;j<(num-i);j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0;j<(2*i-1);j++) {
+//				System.out.print("*");
+//			}
+//			for(int j=0;j<(num-i);j++) {
+//				System.out.print(" ");
+//			}
+//			
+//			System.out.println();
+//		}
+//		
+//		for(int i=num-1;i>0;i--) {
+//			for(int j=0;j<(num-i);j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0;j<(2*i-1);j++) {
+//				System.out.print("*");
+//			}
+//			for(int j=0;j<(num-i);j++) {
+//				System.out.print(" ");
+//			}
+//			
+//			System.out.println();
+//		}
+//		
+//	}
+
+}
